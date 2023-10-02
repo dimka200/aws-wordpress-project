2d3d2e88140232f03139d06aa7f1d1ff7bac65d9
Dimitar Krachunov <dimitarkrachunovkbc@gmail.com>
2023-10-02 10:46:11 +0300

    feature/002

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
     region = "eu-central-1" # Change to your desired region
-    shared_credentials_file = "./aws/credentials"
+    shared_credentials_files = "./aws/credentials"
 }
